779b56817a07a0a9ea49ce744763bdf85008a5dd
saladawid <dawidsalamon@gmail.com>
2022-05-31 08:11:38 +0200

    Basic configuration has been added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+import {Container} from 'react-bootstrap';
+import {HomeScreen} from './screens/HomeScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export const App = () => {
+    return (
+        <>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <HomeScreen/>
+                </Container>
+            </main>
+            <Footer/>
+
+        </>
+    );
+};
+
+
+
